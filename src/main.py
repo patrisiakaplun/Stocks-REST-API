8b2d4131ed40e9b08eee7ceddeb2b6e7d01cb2e6
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2024-06-27 21:08:57 +0300

    adding routers

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+from routers.update_stock import update_stock_router
+from routers.get_stock import get_stock_router
+
+app = FastAPI()
+
+app.include_router(get_stock_router)
+app.include_router(update_stock_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
---
+++ src/routers/get_stock.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+get_stock_router = APIRouter()
+
+@get_stock_router.get("/stock/{stock_symbol}")
+async def get_stock(stock_symbol: str):
+    return
