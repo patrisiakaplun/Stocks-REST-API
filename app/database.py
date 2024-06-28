0950dbe1a75544cd11e325206353bb21c46261b6
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2024-06-28 07:56:55 +0300

    fixing database

--- app/database.py
+++ app/database.py
@@ -7,3 +7,10 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:09071999@localhost/stocks_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
