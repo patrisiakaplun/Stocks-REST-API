087c47b279c3d6e414e573205d72d8a29e6acb43
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2024-06-28 08:06:36 +0300

    fixing models

--- app/models/event.py
+++ app/models/event.py
@@ -1,11 +1,11 @@
 from app.database import Base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, TIMESTAMP, text
 
 class Event(Base):
     __tablename__ = "events"
 
     id = Column(Integer, primary_key=True, index=True)
-    timestamp = Column(String)
+    timestamp = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
     endpoint = Column(String)
     method = Column(String)
     status_code = Column(Integer)
--- app/models/order.py
+++ app/models/order.py
@@ -1,10 +1,10 @@
 from app.database import Base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Float, Integer, String, TIMESTAMP, text
 
 class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
     stock_symbol = Column(String)
-    amount = Column(Integer)
-    timestamp = Column(String)
+    amount = Column(Float)
+    timestamp = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
