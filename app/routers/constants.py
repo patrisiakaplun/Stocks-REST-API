1a697879c70a043e64fe96e7947e4bb8c86c43b8
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2024-06-29 22:14:48 +0300

    adding constants.py file

---
+++ app/routers/constants.py
@@ -0,0 +1,2 @@
+POLYGON_API_KEY = "bs1n5Vdqoi_NOvmCZ_85rrcvtFnYN3vm"
+POLYGON_BASE_URL = "https://api.polygon.io/v1/open-close"
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 from typing import Dict
-from datetime import datetime
 
 class StockBase(BaseModel):
     symbol: str
