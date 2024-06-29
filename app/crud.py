90824056f21f1d1e40610269258d2d861303b162
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2024-06-29 13:09:55 +0300

    adding crud.py file

--- app/crud.py
+++ app/crud.py
@@ -1,34 +1,37 @@
 # app/crud.py
 from sqlalchemy.orm import Session
-from app import models, schemas
+from app import schemas
+from app.models.stock import Stock
+from app.models.order import Order
+from app.models.event import Event
 
 def get_stock_by_symbol(db: Session, symbol: str):
-    return db.query(models.Stock).filter(models.Stock.symbol == symbol).first()
+    return db.query(Stock).filter(Stock.symbol == symbol).first()
 
-def create_stock(db: Session, stock: schemas.StockCreate):
-    db_stock = models.Stock(**stock.dict())
+def create_stock(db: Session, stock: schemas.StockBase):
+    db_stock = Stock(**stock.model_dump())
     db.add(db_stock)
     db.commit()
     db.refresh(db_stock)
     return db_stock
 
 def update_stock_amount(db: Session, symbol: str, amount: int):
-    db_stock = db.query(models.Stock).filter(models.Stock.symbol == symbol).first()
+    db_stock = db.query(Stock).filter(Stock.symbol == symbol).first()
     if db_stock:
         db_stock.amount += amount
         db.commit()
         db.refresh(db_stock)
     return db_stock
 
-def create_order(db: Session, order: schemas.OrderCreate):
-    db_order = models.Order(**order.dict())
+def create_order(db: Session, order: schemas.OrderBase):
+    db_order = Order(**order.model_dump())
     db.add(db_order)
     db.commit()
     db.refresh(db_order)
     return db_order
 
-def create_event(db: Session, event: schemas.EventCreate):
-    db_event = models.Event(**event.dict())
+def create_event(db: Session, event: schemas.EventBase):
+    db_event = Event(**event.model_dump())
     db.add(db_event)
     db.commit()
     db.refresh(db_event)
