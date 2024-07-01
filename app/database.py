8e7abf70c84e2627724c5a1ea9eae7abcc5c5568
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2024-07-01 18:01:11 +0300

    fixing database.py and main.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user1:user1@localhost/stocks_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user1:user1@db:5432/stocks_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -10,4 +10,4 @@ Base.metadata.create_all(bind=engine)
 app.include_router(stocks_router)
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host="127.0.0.1", port=8080)
+    uvicorn.run('main:app', host="127.0.0.1", port=8000)
